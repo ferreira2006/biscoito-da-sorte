c01db83c7e6905811d94727d84c58d0fe1be4269
Marcos Ferreira <ferreira2006@gmail.com>
2025-09-05 18:01:43 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ let historico = [];
 
 // Lista de frases
 let frases = [];
-fetch('../assets/frases.txt')
+fetch('https://ferreira2006.github.io/biscoito-da-sorte/assets/frases.txt')
   .then(response => response.text())
   .then(data => {
     frases = data.split('\n');
